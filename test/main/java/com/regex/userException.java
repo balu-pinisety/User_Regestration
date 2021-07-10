bdc05e3679f85e97ebf1bcde24fd66b0582ab71f
balu-pinisety <manikantapandu225@gmail.com>
2021-07-10 18:29:53 +0530

    Added: File for Exception case for Invalid input

---
+++ test/main/java/com/regex/userException.java
@@ -0,0 +1,13 @@
+package com.regex;
+
+/**
+ * @author BALU
+ * Provides Custom Message for invalid inputs
+ */
+public class userException extends Exception {
+
+	public userException(String message) {
+		super(message);
+	}
+
+}
